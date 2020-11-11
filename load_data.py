5b2e6e67c36009076e75d4d44fe4616fdec8a176
EugeneSel <youdjin.sel15@gmail.com>
2020-11-11 13:30:22 +0100

    Added bad responses handling while loading data

--- load_data.py
+++ load_data.py
@@ -14,14 +14,22 @@ PARAMS = {
 
 def load_data():
     r_total = requests.get(url=URL_TOTAL, params=PARAMS) 
-    data_total = r_total.json()
+    if r_total.ok:
+        data_total = r_total.json()
 
-    df_total = pd.DataFrame(data_total)
+        df_total = pd.DataFrame(data_total)
+    else:
+        print(r_total.reason)
+        df_total = None
 
     r_daily_cases = requests.get(url=URL_DAILY_CASES, params=PARAMS) 
-    data_daily_cases = r_daily_cases.json()
+    if r_daily_cases.ok:
+        data_daily_cases = r_daily_cases.json()
 
-    df_daily_cases = pd.DataFrame(data_daily_cases)
+        df_daily_cases = pd.DataFrame(data_daily_cases)
+    else:
+        print(r_total.reason)
+        df_daily_cases = None
 
     return df_total, df_daily_cases
 
@@ -32,5 +40,7 @@ if __name__ == '__main__':
 
     df_total, df_daily_cases = load_data()
     
-    display(df_total.head())
-    display(df_daily_cases.head())
+    if df_total is not None:
+        display(df_total.head())
+    if df_daily_cases is not None:
+        display(df_daily_cases.head())
